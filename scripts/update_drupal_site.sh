9c1dea18eb16e8ee7c33c9080f484858ab3db975
Frank Crijns <frankcrijns@outlook.com>
2014-04-11 15:59:09 +0200

    [Frank] updated rsync command

--- scripts/update_drupal_site.sh
+++ scripts/update_drupal_site.sh
@@ -1,6 +1,6 @@
 echo 'Remove old site content'
 echo 'Copy new site content'
-rsync -arzv --delete ./docroot/ /var/www/cetelembe/
+rsync -arzv --delete ./docroot/* /var/www/cetelembe/
 echo 'Copy settings file'
 cp /var/www/cetelembe/sites/default/default.settings.php /var/www/cetelembe/sites/default/settings.php
 cd /var/www/cetelembe

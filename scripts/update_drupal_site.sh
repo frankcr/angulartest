30f566412399780218e79db2578380ee23db2301
Frank Crijns <frankcrijns@outlook.com>
2014-04-11 14:40:48 +0200

    [Frank] Remove old files with rsync

--- scripts/update_drupal_site.sh
+++ scripts/update_drupal_site.sh
@@ -1,7 +1,6 @@
 echo 'Remove old site content'
-rm -rf /var/www/cetelembe/*
 echo 'Copy new site content'
-rsync -azv docroot/ /var/www/cetelembe/
+rsync -azv --delete ./docroot/ /var/www/cetelembe/
 echo 'Copy settings file'
 cp /var/www/cetelembe/sites/default/default.settings.php /var/www/cetelembe/sites/default/settings.php
 cd /var/www/cetelembe

7078173fb9574904499038991421dfd3df52951b
Frank Crijns <frankcrijns@outlook.com>
2014-04-11 14:02:01 +0200

    [Frank] Added comments to shell script

--- scripts/update_drupal_site.sh
+++ scripts/update_drupal_site.sh
@@ -1,7 +1,13 @@
+rem Remove old site content
 rm -rf /var/www/cetelembe/*
+rem Copy new site content
 cp -R docroot/ /var/www/cetelembe/
+rem Copy settings file
 cp /var/www/cetelembe/sites/default/default.settings.php /var/www/cetelembe/sites/default/settings.php
 cd /var/www/cetelembe
+rem Update database
 drush updb -y
+rem Revert features
 drush fra -y
+rem Clear cache
 drush cc all

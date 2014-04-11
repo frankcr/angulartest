c50cb6fc8303257450a6d4b29f2f0f7a615cd043
Frank Crijns <frankcrijns@outlook.com>
2014-04-11 15:52:20 +0200

    [Frank] removed chmod

--- scripts/update_drupal_site.sh
+++ scripts/update_drupal_site.sh
@@ -1,8 +1,6 @@
 echo 'Remove old site content'
 echo 'Copy new site content'
 rsync -azv --delete ./docroot/ /var/www/cetelembe/
-echo 'Change permissions'
-chmod -R 775 /var/www/cetelembe/
 echo 'Copy settings file'
 cp /var/www/cetelembe/sites/default/default.settings.php /var/www/cetelembe/sites/default/settings.php
 cd /var/www/cetelembe

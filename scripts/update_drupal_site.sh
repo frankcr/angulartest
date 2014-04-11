d9594d2282dd7688a033d555a98cf0b0cea5eebc
Frank Crijns <frankcrijns@outlook.com>
2014-04-11 14:36:25 +0200

    [Frank] Use rsync instead of cp

--- scripts/update_drupal_site.sh
+++ scripts/update_drupal_site.sh
@@ -1,7 +1,7 @@
 echo 'Remove old site content'
 rm -rf /var/www/cetelembe/*
 echo 'Copy new site content'
-cp -R docroot/* /var/www/cetelembe/
+rsync -azv docroot/ /var/www/cetelembe/
 echo 'Copy settings file'
 cp /var/www/cetelembe/sites/default/default.settings.php /var/www/cetelembe/sites/default/settings.php
 cd /var/www/cetelembe

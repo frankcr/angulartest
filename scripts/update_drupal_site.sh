3d796e761c4bcca461cc7de5baadeaf9a0e435fe
Frank Crijns <frankcrijns@outlook.com>
2014-04-11 13:58:00 +0200

    [Frank] Added drush commands to update script

--- scripts/update_drupal_site.sh
+++ scripts/update_drupal_site.sh
@@ -1,2 +1,7 @@
 rm -rf /var/www/cetelembe/*
 cp -R docroot/ /var/www/cetelembe/
+cp /var/www/cetelembe/sites/default/default.settings.php /var/www/cetelembe/sites/default/settings.php
+cd /var/www/cetelembe
+drush updb -y
+drush fra -y
+drush cc all
